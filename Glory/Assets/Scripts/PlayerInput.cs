de49dfec6cf9fd860966255b990b7d5790296278
DariaYudina004 <yudina.daria.antonovna@gmail.com>
2023-05-10 17:53:48 +0300

    Further development stage.
    OpenXR plugin has been added. Oculus Hands Package has been imported. The left and right controllers have been adjusted.Getting Started with Hand Animation. Writing a new PlayerInput script.
    Linked movement to the right and left controller.
    Items that can be contacted have been added. The functions of taking, pulling and throwing objects have been adjusted.
    Weapon mechanics have been tweaked.

---
+++ Glory/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private const string GRIP = "Grip";
+    private const string TRIGGER = "Trigger";
+
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private InputActionProperty _gripAction;
+    [SerializeField] private InputActionProperty _activateAction;
+
+    private void Update()
+    {
+        var gripValue = _gripAction.action.ReadValue<float>();
+        var actionValue = _activateAction.action.ReadValue<float>();
+
+        _animator.SetFloat(name: "Grip", gripValue);
+        _animator.SetFloat(name: "Trigger", actionValue);
+
+    }
+}

3a7bded76639efe1e9a7c6ec11bd4f29c6b1b4f2
DariaYudina004 <yudina.daria.antonovna@gmail.com>
2023-09-09 15:24:42 +0300

    fix

--- Glory/Assets/Scripts/GlobalControl.cs
+++ Glory/Assets/Scripts/GlobalControl.cs
@@ -5,6 +5,7 @@ public class GlobalControl : MonoBehaviour
     public static float DelayLevel;// Задержка в генерации и создании новых объектов, предстовляет соьой показатеь уровня сложности
     public static int healthOfPlayer;
     public static int countofHit;
+    
 
 }
 
